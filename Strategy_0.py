a3ed5ef9a400f9285d5196dc31d3897e8bfea7c3
hermes926 <hermes926@gmail.com>
2021-08-30 22:03:44 +0800

    Long Short both

--- Strategy_0.py
+++ Strategy_0.py
@@ -5,14 +5,17 @@ import matplotlib.pyplot as plt
 
 series = pd.date_range(start='2020-01-01', end='2020-12-31', freq='D')
 asset = [ [2000, "-1", 0] for i in range(50)]
+neg_asset = [ [-2000, "-1", 0] for i in range(50)]
+
+
 rawdata = pd.read_csv('../mapping.csv')
-rawdata = rawdata["ticker"].array[127:1085]
+rawdata = rawdata["ticker"].array[130:1085]
+# Select more static stocks
 mapped = { i: 0 for i in rawdata}
 # read in data
 
 cnt = 0
 earning = [[]]
-# earning_single = [[]] * 10
 last = pd.DataFrame()
 
 for index in series:
@@ -28,6 +31,7 @@ for index in series:
         sum = 0
         for i in range(50):
             sum += asset[i][0]
+            sum += neg_asset[i][0]
         earning.append([index, sum])
         # update for the plot
 
@@ -42,7 +46,7 @@ for index in series:
                         asset[i][1] = j
                         asset[i][2] = asset[i][0] / today['open_'].values[0]
                         break 
-            # No holding
+            # Long and No holding
             
             else:
                 today = time_data.loc[time_data['identifier'] == asset[i][1]]
@@ -54,11 +58,35 @@ for index in series:
                     mapped[asset[i][1]] = 0
                     asset[i][1] = "-1"
                     asset[i][2] = 0
-            # Holding 
+            # Long Holding 
         
+            if neg_asset[i][1] == "-1":
+                for j in rawdata:
+                    today = time_data.loc[time_data['identifier'] == j]
+                    yest = last.loc[last['identifier'] == j]
+                    if mapped[j] == 0 and not today.empty and not yest.empty and today['MA_5'].values[0] < today['MA_20'].values[0] and yest['MA_20'].values[0] < yest['MA_5'].values[0]:
+                        mapped[j] = 1
+                        neg_asset[i][1] = j
+                        neg_asset[i][2] = neg_asset[i][0] / today['open_'].values[0]
+                        break 
+            # Short and no holding
+            
+            else:
+                today = time_data.loc[time_data['identifier'] == neg_asset[i][1]]
+                if not today.empty:
+                    neg_asset[i][0] = neg_asset[i][2] * today['open_'].values[0]    
+
+                yest = last.loc[last['identifier'] == neg_asset[i][1]]
+                if not today.empty and not yest.empty and today['MA_5'].values[0] > today['MA_20'].values[0] and yest['MA_20'].values[0] > yest['MA_5'].values[0]:
+                    mapped[neg_asset[i][1]] = 0
+                    neg_asset[i][1] = "-1"
+                    neg_asset[i][2] = 0
+            # Short and Holding 
+        
+
+
         last = time_data
 
-# print(earning)
 series = pd.DataFrame(earning, columns =['date', 'earning'])
 series['date'] = pd.to_datetime(series['date'])
 plt.plot(series['date'], series['earning'])
